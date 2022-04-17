b7fd9b03aa81cb8ef92dc9527827a556accbabb6
unknown <efbauerg@yahoo.com.mx>
2022-04-16 23:23:42 -0500

    comm jsiv.14

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -110,6 +110,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -118,6 +120,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for(var i = 0; i < usuarios.length; i++) {
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -127,6 +133,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < usuario.posts.length; i++) {
+    suma = suma + usuario.posts[i].likes;
+  }
+  return
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -139,7 +150,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = funcion() {
+    return this.precio - ( this.precio * this.porcentajeDeDescuento );
+  }
+  return producto
 }
 
 // No modificar nada debajo de esta línea
